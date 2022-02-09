7588ddb4c212e34368460c0a29418608d1f364c2
Tymur Renhach <tymurrenhach@gmail.com>
2022-02-09 16:56:20 +0200

    Header and scroll for test was done

--- js/script.js
+++ js/script.js
@@ -4,6 +4,47 @@
 
 (function ($) {
   $(document).ready(function () {
-    
+    $('#multiscroll').multiscroll({
+      verticalCentered: true,
+      scrollingSpeed: 700,
+      easing: 'easeInQuart',
+      easingcss3: 'ease-out',
+      menu: false,
+      sectionsColor: [],
+      navigation: false,
+      navigationPosition: 'right',
+      navigationColor: '#000',
+      navigationTooltips: [],
+      loopBottom: false,
+      loopTop: false,
+      css3: false,
+      paddingTop: 0,
+      paddingBottom: 0,
+      normalScrollElements: null,
+      scrollOverflow: false,
+      scrollOverflowOptions: null,
+      keyboardScrolling: true,
+      touchSensitivity: 5,
+
+      //responsive
+      responsiveWidth: 0,
+      responsiveHeight: 0,
+      responsiveExpand: false,
+
+      // Custom selectors
+      sectionSelector: '.ms-section',
+      leftSelector: '.ms-left',
+      rightSelector: '.ms-right',
+
+      //events
+      onLeave: function (index, nextIndex, direction) { },
+      afterLoad: function (anchorLink, index) { },
+      afterRender: function () { },
+      afterResize: function () { },
+    });
+    $('.hamburger').on('click', function () {
+      $('.hamburger').toggleClass('active');
+      $('.hamburger-menu').toggleClass('open');
+    });
   });
 })(jQuery);
