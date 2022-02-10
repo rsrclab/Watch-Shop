1218cc58d4ba9a0a5b0a4a7db2d66065f9e2b340
Tymur Renhach <tymurrenhach@gmail.com>
2022-02-10 16:29:25 +0200

    test task was done

--- js/script.js
+++ js/script.js
@@ -9,39 +9,20 @@
       scrollingSpeed: 700,
       easing: 'easeInQuart',
       easingcss3: 'ease-out',
-      menu: false,
       sectionsColor: [],
       navigation: false,
-      navigationPosition: 'right',
-      navigationColor: '#000',
-      navigationTooltips: [],
-      loopBottom: false,
-      loopTop: false,
-      css3: false,
-      paddingTop: 0,
-      paddingBottom: 0,
-      normalScrollElements: null,
-      scrollOverflow: false,
-      scrollOverflowOptions: null,
       keyboardScrolling: true,
       touchSensitivity: 5,
 
-      //responsive
-      responsiveWidth: 0,
-      responsiveHeight: 0,
-      responsiveExpand: false,
+      // //responsive
+      responsiveWidth: 767,
 
       // Custom selectors
       sectionSelector: '.ms-section',
       leftSelector: '.ms-left',
       rightSelector: '.ms-right',
-
-      //events
-      onLeave: function (index, nextIndex, direction) { },
-      afterLoad: function (anchorLink, index) { },
-      afterRender: function () { },
-      afterResize: function () { },
     });
+
     $('.hamburger').on('click', function () {
       $('.hamburger').toggleClass('active');
       $('.hamburger-menu').toggleClass('open');
